370e3e9eedd19f8ab2d2280ad5bd1c4612b1ef24
Grace Westerman <gracewes@tamu.edu>
2020-01-25 16:31:50 -0600

    made request to summarize and printed out summary to console, need to add in key as variable 'key'

--- background.js
+++ background.js
@@ -1,8 +1,23 @@
+const Http = new XMLHttpRequest();
+
+// Need proxy to get around cors errors
+const proxyurl = "https://cors-anywhere.herokuapp.com/";
+const smmryUrl = `https://api.smmry.com/&SM_API_KEY=${key}`;
+
 chrome.runtime.onInstalled.addListener(function() {
     console.log("installed");
   });
 
   chrome.browserAction.onClicked.addListener(function(tab) {
     console.log(tab.url);
+    console.log(smmryUrl);
     console.log("sending to smmry");
+    var url = proxyurl + smmryUrl + `&SM_URL=${tab.url}`;
+    console.log(url);
+    Http.open("GET", url);
+    Http.send();
+
+    Http.onreadystatechange = (e) => {
+        console.log(Http.responseText)
+    }
   });
